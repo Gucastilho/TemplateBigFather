602523f305ac4bd6eb19ab5d2e8773f99e80351a
Gustavo Castilho <gucastilho23@gmail.com>
2023-09-12 22:15:20 -0300

    input

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,11 +1,25 @@
+import foto1 from "../../../assets/icon.png";
 import React from "react";
-import { Text,TextInput,TouchableOpacity,View, StyleSheet} from "react-native";
-import {styles} from "./styles";
+import {
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  StyleSheet,
+  Image,
+} from "react-native";
+import { styles } from "./styles";
 
-export function Home(){
-    return(
-        <View style={styles.container}>
-            <Text style={styles.textTitle}>Teste</Text>
-        </View>
-    )
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.textTitle}>Teste</Text>
+      </View>
+      <View style={styles.boby}>
+        <TextInput style={styles.input} keyboardType="numeric"></TextInput>
+        
+      </View>
+    </View>
+  );
 }
--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -11,6 +11,36 @@ container:{
 textTitle:{
 color: "#FFF",
 
+},
+
+header:{
+backgroundColor: "red",
+
+
+},
+
+boby:{
+flex: 1,
+width: '100%',
+flexDirection: 'row',
+marginTop: 36,
+marginBottom : 42,
+marginRight: 40,
+
+
+
+},
+
+input:{
+    backgroundColor: "green",
+    width: 100,
+    height: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    color: "#FFF",
+    marginRight: 60,
+
+
 }
 
 
