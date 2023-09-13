d311ea8ad93228405146a338aa50f86a23f1f084
Gustavo Castilho <gucastilho23@gmail.com>
2023-09-12 21:35:49 -0300

    app-funcionando

---
+++ src/screens/Home/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text,TextInput,TouchableOpacity,View, StyleSheet} from "react-native";
+import {styles} from "./styles";
+
+export function Home(){
+    return(
+        <View style={styles.container}>
+            <Text style={styles.textTitle}>Teste</Text>
+        </View>
+    )
+}
---
+++ src/screens/Home/styles.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+
+export const styles = StyleSheet.create ({
+
+container:{
+    backgroundColor: "#373737",
+    flex: 1,
+},
+
+textTitle:{
+color: "#FFF",
+
+}
+
+
+
+})
